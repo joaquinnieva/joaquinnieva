cd0f390d5bb90fdd63a98d6fbea33278962b87e0
jqndev <only.play.blued@gmail.com>
2021-10-12 00:43:00 -0300

    arr

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import Inicio from "./pages/Inicio";
 import Habilidades from "./pages/Habilidades";
 import Biografia from "./pages/Biografia";
@@ -14,30 +14,7 @@ function App() {
       <Portafolio />
       <Biografia />
     </>
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> 4e3fda5 (Initialize project using Create React App)
-  );
+  )
 }
 
 export default App;
